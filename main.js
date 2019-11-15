942488fc37e26c975f6d712b5fc9ffe954d47496
Aleksandr Beloushkin <aleks.beloushkin@yahoo.com>
2019-11-15 09:55:48 +0300

    Minor fixes

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 // Modules to control application life and create native browser window
-const { app, Menu, Tray, BrowserWindow } = require('electron')
-const path = require('path')
+const { app, Menu, Tray, BrowserWindow } = require("electron");
+const path = require("path");
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
-let tray
+let mainWindow;
+let tray;
 
 function createWindow() {
   // Create the browser window.
@@ -13,57 +13,49 @@ function createWindow() {
     width: 800,
     height: 800,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, "preload.js"),
       nodeIntegration: true
     }
-  })
-
-  //tray = new Tray('./cam.png')
-
-  // tray.on('click', () => {
-  //   win.isVisible() ? win.hide() : win.show()
-  // })
-  // mainWindow.on('show', () => {
-  //   tray.setHighlightMode('always')
-  // })
-  // mainWindow.on('hide', () => {
-  //   tray.setHighlightMode('never')
-  // })
+  });
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
-  //if we need to open devtools
-  //mainWindow.webContents.openDevTools();
-  mainWindow.webContents.on('devtools-opened', () => {
+  mainWindow.loadFile("index.html");
+  // set full screen if need
+  //mainWindow.setFullScreen(true);
+  // if we need to open devtools
+  /*
+  mainWindow.webContents.openDevTools();
+  mainWindow.webContents.on("devtools-opened", () => {
     setImmediate(() => {
       // do whatever you want to do after dev tool completely opened here
       mainWindow.focus();
     });
   });
+  */
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
+  mainWindow.on("closed", function() {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null
-  })
+    mainWindow = null;
+  });
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on("ready", createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
+app.on("window-all-closed", function() {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') app.quit()
-})
+  if (process.platform !== "darwin") app.quit();
+});
 
-app.on('activate', function () {
+app.on("activate", function() {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) createWindow()
-})
+  if (mainWindow === null) createWindow();
+});
--- renderer.js
+++ renderer.js
@@ -1,9 +1,10 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-var fs = require('fs');
-var requestify = require('requestify');
-var webcam = require('./lib/webcam');
+var fs = require("fs");
+//require("./lib/const");
+var requestify = require("requestify");
+var webcam = require("./lib/webcam");
 var body = document.body;
 var header = document.getElementsByClassName("main-header")[0];
 var normalheader = document.getElementsByClassName("normal-header")[0];
@@ -11,85 +12,89 @@ var offlineheader = document.getElementsByClassName("offline-header")[0];
 
 var dangers = [];
 
-const ImageDataURI = require('image-data-uri');
-
+const ImageDataURI = require("image-data-uri");
+const baseUrl = "http://192.168.107.170";
+const port = 8898;
 
 function writeImageFile(URI, path) {
-    //console.log(URI);
+  //console.log(URI);
 
-    try {
-        ImageDataURI.outputFile(URI, path);
-    } catch (e) { alert(e); }
+  try {
+    ImageDataURI.outputFile(URI, path);
+  } catch (e) {
+    alert(e);
+  }
 }
 
-
 function sendImageToServer(URI) {
-    requestify.post('http://localhost:5000/predimg', {
-        "img": URI
+  requestify
+    .post(`${baseUrl}:${port}/predimg`, {
+      img: URI
     })
-        .then(function (response) {
-            // Get the response body (JSON parsed or jQuery object for XMLs)
-            toggleDisconnectedStatus(false);
-            answer = response.getBody()["class_name"];
-            console.log(answer);
-
-        })
-        .fail(function (response) {
-            toggleDisconnectedStatus(true);
-            console.log(response.getCode()); // Some error code such as, for example, 404
-        });
+    .then(function(response) {
+      // Get the response body (JSON parsed or jQuery object for XMLs)
+      toggleDisconnectedStatus(false);
+      answer = response.getBody()["class_name"];
+      console.log(answer);
+    })
+    .fail(function(response) {
+      toggleDisconnectedStatus(true);
+       // Some error code such as, for example, 404
+      console.log(response.getCode());
+    });
 }
 
 function sendImageToServerAndCheckImmediate(URI) {
-    requestify.post('http://localhost:5000/predimg', {
-        "img": URI
+  requestify
+    .post(`${baseUrl}:${port}/predimg`, {
+      img: URI
     })
-        .then(function (response) {
-            // Get the response body (JSON parsed or jQuery object for XMLs)
-            toggleDisconnectedStatus(false);
-            answer = response.getBody()["class_name"];
-            console.log(answer);
-            toggleDangerStatus(answer);
-        })
-        .fail(function (response) {
-            toggleDisconnectedStatus(true);
-            console.log(response.getCode()); // Some error code such as, for example, 404
-        });
+    .then(function(response) {
+      // Get the response body (JSON parsed or jQuery object for XMLs)
+      toggleDisconnectedStatus(false);
+      answer = response.getBody()["class_name"];
+      console.log(answer);
+      toggleDangerStatus(answer);
+    })
+    .fail(function(response) {
+      toggleDisconnectedStatus(true);
+      console.log(response.getCode()); 
+    });
 }
 
 function toggleDangerStatus(answer) {
-    if (answer == "Danger") {
-        body.classList.add("danger-red");
-        header.classList.remove("hidden");
-        normalheader.classList.add("hidden");
-    } else {
-        body.classList.remove("danger-red");
-        header.classList.add("hidden");
-        normalheader.classList.remove("hidden");
-    }
+  if (answer == "Danger") {
+    body.classList.add("danger-red");
+    header.classList.remove("hidden");
+    normalheader.classList.add("hidden");
+  } else {
+    body.classList.remove("danger-red");
+    header.classList.add("hidden");
+    normalheader.classList.remove("hidden");
+  }
 }
 
 function toggleDisconnectedStatus(disconnected) {
-    if (disconnected) {
-        body.classList.remove("danger-red");
-        body.classList.add("neutral-white");
-        offlineheader.classList.remove("hidden");
-    } else {
-        body.classList.remove("neutral-white");
-        offlineheader.classList.add("hidden");
-    }
+  if (disconnected) {
+    body.classList.remove("danger-red");
+    body.classList.add("neutral-white");
+    offlineheader.classList.remove("hidden");
+  } else {
+    body.classList.remove("neutral-white");
+    offlineheader.classList.add("hidden");
+  }
 }
 
 function checkStatus() {
-    requestify.get('http://localhost:5000/check')
-        .then(function (response) {
-            // Get the response body
-            toggleDisconnectedStatus(false);
-            answer = response.getBody()["class_name"];
-            toggleDangerStatus(answer);
-        })
-        .fail(function (response) {
-            toggleDisconnectedStatus(true);
-            console.log(response.getCode()); // Some error code such as, for example, 404
-        });
+  requestify
+    .get(`${baseUrl}:${port}/check`)
+    .then(function(response) {
+      // Get the response body
+      toggleDisconnectedStatus(false);
+      answer = response.getBody()["class_name"];
+      toggleDangerStatus(answer);
+    })
+    .fail(function(response) {
+      toggleDisconnectedStatus(true);
+      console.log(response.getCode()); 
 }
